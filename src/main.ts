658625fa833191048d07e0bc9a7dd8ea63fd705a
juanfe <lassojuanfe@gmail.com>
2024-10-15 08:31:47 -0500

    Ajustes menores de scripts

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,20 @@ import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Usar filtros globales para manejo de excepciones
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Middleware de morgan para logging
   app.use(morgan('combined'));
-  await app.listen(3000, '0.0.0.0');
+
+  // Usar puerto desde variables de entorno
+  const port = process.env.PORT || 3000;
+  
+  // Iniciar el servidor en la dirección 0.0.0.0 y el puerto dinámico
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
