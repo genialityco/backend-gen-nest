858e2a3b3a30a8ae251c11f3bd4889e2198cde3e
balderban <balderban@gmail.com>
2024-10-24 16:06:07 -0500

    fixing file upload

--- src/utils/UploadController.ts
+++ src/utils/UploadController.ts
@@ -1,43 +1,39 @@
 import {
-    Controller,
-    Post,
-    UploadedFile,
-    UseInterceptors,
-    BadRequestException,
-  } from '@nestjs/common';
-  import { FileInterceptor } from '@nestjs/platform-express';
-  import { initializeFirebaseAdmin } from 'src/config/firebase-admin.config';
-  
-  @Controller('upload')
-  export class UploadController {
-    constructor(private readonly firebaseAdminService: initializeFirebaseAdmin) {}
-  
-    @Post('image')
-    @UseInterceptors(FileInterceptor('file'))
-    async uploadImage(@UploadedFile() file: Express.Multer.File) {
-      if (!file) {
-        throw new BadRequestException('No file uploaded');
-      }
-  
-      const bucket = this.firebaseAdminService.getStorage().bucket();
-      const blob = bucket.file(Date.now() + '-' + file.originalname);
-  
-      const blobStream = blob.createWriteStream({
-        metadata: {
-          contentType: file.mimetype,
-        },
-      });
-  
-      return new Promise((resolve, reject) => {
-        blobStream.on('error', (error) => reject(`Unable to upload image, something went wrong: ${error.message}`));
-        blobStream.on('finish', () => {
-          // Get public URL
-          const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-          resolve({ imageUrl: publicUrl });
-        });
-  
-        blobStream.end(file.buffer);
-      });
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { initializeFirebaseAdmin } from 'src/config/firebase-admin.config';
+import { diskStorage, memoryStorage } from 'multer';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly firebaseAdminService: initializeFirebaseAdmin) {}
+
+  @Post('image')
+  @UseInterceptors(FileInterceptor('file', { storage: memoryStorage() }))
+  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('No file uploaded');
     }
+
+    const bucket = this.firebaseAdminService.getStorage().bucket();
+    const blob = bucket.file('Y' + Date.now() + '-' + file.originalname);
+
+    await blob.save(file.buffer, {
+      contentType: file.mimetype,
+      gzip: true,
+    });
+
+    await blob.makePublic();
+
+    return new Promise((resolve, reject) => {
+      // Get public URL
+      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+      resolve({ imageUrl: publicUrl });
+    });
   }
-  
+}
